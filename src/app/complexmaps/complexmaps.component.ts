a3c4c265077c9281f0efbca2907820f3e79da821
NabeelSaleem333 <nabeel.saleem333@gmail.com>
2020-08-13 11:40:40 +0500

    E-16 Map Done

--- src/app/complexmaps/complexmaps.component.ts
+++ src/app/complexmaps/complexmaps.component.ts
@@ -9,6 +9,15 @@ import { environment } from 'src/environments/environment';
 export class ComplexmapsComponent implements OnInit {
   // Objects
   map: mapboxgl.Map;
+  // -
+  geojson = {
+    coordinates: [
+      [33.652982, 72.877392],
+      [33.652946, 72.898120],
+      [33.637977, 72.898034],
+      [33.638049, 72.877349],
+    ],
+  };
   // Arrays
   // Variables
   // Counters
@@ -17,6 +26,7 @@ export class ComplexmapsComponent implements OnInit {
 
   ngOnInit() {
     this.initMapBox();
+    this.complexOverlay();
   }
 
   /*
@@ -27,8 +37,8 @@ export class ComplexmapsComponent implements OnInit {
       accessToken: environment.mapbox.accessToken,
       container: 'mapcomplexId',
       style: 'mapbox://styles/mapbox/streets-v11',
-      center: [73.056229, 33.722319], // starting position [lng, lat]
-      zoom: 10, // starting zoom
+      center: [this.geojson.coordinates[0][1], this.geojson.coordinates[0][0]], // starting position [lng, lat]
+      zoom: 14, // starting zoom
     });
     this.fullScreenControl();
     this.ZoomInOutControl();
@@ -61,9 +71,45 @@ export class ComplexmapsComponent implements OnInit {
   Add complex overlay on mapbox
   */
   complexOverlay() {
-    // this.map.addSource('mine', {
-    //   type: 'geojson',
-    //   data: {this.geojson}
-    // });
-  }
+
+    const slider = document.getElementById('slider');
+    const sliderValue = document.getElementById('slider-value');
+
+    this.map.on('load', () => {
+    this.map.addSource('myImageSource', {
+      type: 'image',
+      url: 'assets/maps/E-16-crop.png',
+      coordinates: [
+        [this.geojson.coordinates[0][1], this.geojson.coordinates[0][0]],
+        [this.geojson.coordinates[1][1], this.geojson.coordinates[1][0]],
+        [this.geojson.coordinates[2][1], this.geojson.coordinates[2][0]],
+        [this.geojson.coordinates[3][1], this.geojson.coordinates[3][0]],
+      ]
+    });
+
+    this.map.addLayer({
+      id: 'myImageSource',
+      source: 'myImageSource',
+      type: 'raster',
+      paint: {
+        'raster-opacity': 1,
+      },
+    });
+    slider.addEventListener('input', (e) => {
+      // Adjust the layers opacity. layer here is arbitrary - this could
+      // be another layer name found in your style or a custom layer
+      // added on the fly using `addSource`.
+      // e.target;
+      const input = (document.getElementById('slider') as HTMLInputElement)
+        .value;
+      this.map.setPaintProperty(
+        'myImageSource',
+        'raster-opacity',
+        parseInt('' + input + '', 10) / 100
+      );
+      // Value indicator
+      sliderValue.textContent = '' + input + '' + '%';
+    });
+  });
+}
 }
